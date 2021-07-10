db77cf5666cf0ea3655151d67af1d97bf131a327
Kanika Sharma <kanikasharma1224@gmail.com>
2021-07-10 22:08:16 +0200

    Added Integration Test

--- src/main/java/com/sapient/test/controller/CreditCardProcessorController.java
+++ src/main/java/com/sapient/test/controller/CreditCardProcessorController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +22,23 @@ import com.sapient.test.service.CreditCardService;
 @RequestMapping(value = "/api/v1")
 public class CreditCardProcessorController {
 
+	Logger logger = LoggerFactory.getLogger(CreditCardProcessorController.class);
+	
 	@Autowired
 	CreditCardService service;
 
 	@PostMapping("/cards")
 	public ResponseEntity<CreditCardDetails> addCard(@RequestBody @Valid CreditCardDetails details) {
+		logger.debug("Inside Add card");
 		details.setBalance(0.0);
 		CreditCardDetails crd = service.save(details);
+		logger.info("New Card successfully added ");
 		return new ResponseEntity<CreditCardDetails>(crd,HttpStatus.CREATED);
 	}
 
 	@GetMapping("/cards")
 	public List<CreditCardDetails> getAllCards() {
+		logger.debug("Inside get all cards");
 		return service.getAllCards();
 	}
 
--- src/main/java/com/sapient/test/validations/Luhn10Validator.java
+++ src/main/java/com/sapient/test/validations/Luhn10Validator.java
@@ -3,12 +3,17 @@ package com.sapient.test.validations;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Luhn10Validator implements ConstraintValidator<ValidLuhn10, String> {
 
+	Logger logger = LoggerFactory.getLogger(Luhn10Validator.class);
+	
 	@Override
 	public boolean isValid(String cardNo, ConstraintValidatorContext context) {
 		
-		System.out.println("Inside validator ");
+		logger.debug("Inside Luhn10 validator ");
 		int cardLength = cardNo.length();
 		 
 	    int cardSum = 0;
@@ -29,7 +34,10 @@ public class Luhn10Validator implements ConstraintValidator<ValidLuhn10, String>
 	 
 	        isSecond = !isSecond;
 	    }
-	    return (cardSum % 10 == 0);
+	    boolean result = (cardSum % 10 == 0);
+	    if(!result)
+	    	logger.error("Invalid card number supplied as input");
+	    return result;
 	}
 
 }
